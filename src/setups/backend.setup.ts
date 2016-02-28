c52ff69b7fe991974638b074709cdfa18973333e
BlackDev0222 <BlackDev0222@gmail.com>
2016-02-28 12:36:04 -0700

    update src\setups\backend.setup.ts

---
+++ src/setups/backend.setup.ts
@@ -0,0 +1,30 @@
+import cors from "cors";
+import express, { Express } from "express";
+
+import routes from "../routes";
+
+import { routeMiddleware } from "../middlewares";
+
+import { MESSAGES } from "../consts";
+
+import { ROUTE_VERSION } from "../config";
+
+import { Logger, Env } from "../utils";
+
+const backendSetup = () => {
+  const app: Express = express();
+
+  // middlewares
+  app.use(cors());
+  app.use(express.json());
+  app.use(routeMiddleware);
+
+  // routes
+  app.use(`/api/${ROUTE_VERSION}/`, routes);
+
+  app.listen(Env.getEnvironmentVariable("PORT"), () => {
+    Logger.info(MESSAGES.SERVER.STARTING_SUCCESS);
+  });
+};
+
+export default backendSetup;
